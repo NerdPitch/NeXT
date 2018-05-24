2b909d06e4469486eeff8cf05016c599d598fe16
pmzi <pouya.mozafar@gmail.com>
2018-05-24 16:38:31 +0430

    a little mistake and restructure in step

--- src/core/Step.js
+++ src/core/Step.js
@@ -6,28 +6,6 @@ class Step{
 
     }
 
-    /**
-     * Sets the transform name for the step
-     * 
-     * @param {Transform} transform - the transform of the step
-     * 
-     */
-
-    set transform(transform){
-        this.transform = transform;
-    }
-
-    /**
-     * Sets the transform name for the step
-     * 
-     * @returns {Transform} transform - the transform of the step
-     * 
-     */
-
-    get transform(){
-        return this.transform;
-    }
-
 }
 
 module.exports = Step;
