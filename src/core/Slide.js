84f9199b78debe2be9cdccd32c98567609727c5c
pmzi <pouya.mozafar@gmail.com>
2018-05-24 16:38:51 +0430

    very first structure of Slide has been made

--- src/core/Slide.js
+++ src/core/Slide.js
@@ -1,5 +1,29 @@
+const Step = require('./Step');
+
 class Slide{
 
+    /**
+     * 
+     * @param {Array} steps - Array of steps which are each an instance of Step class
+     * 
+     */
+
+    constructor(steps){
+
+        this.steps = [];
+
+        // Let's only add steps
+
+        for(let step of steps){
+            if(step instanceof Step){
+                this.steps.push(step)
+            }else{
+                throw new Error('First argument should be an array of steps.');
+            }
+        }
+        
+    }
+    
 }
 
 module.exports = Slide;
