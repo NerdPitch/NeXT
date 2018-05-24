93acf325f32bfe857b45da23d63ba3b1ad26196a
pmzi <pouya.mozafar@gmail.com>
2018-05-24 16:28:45 +0430

    very basic structure of the step has been made

--- src/core/Step.js
+++ src/core/Step.js
@@ -1,5 +1,33 @@
 class Step{
 
+    constructor(transform){
+
+        this.transform = transform || null;
+
+    }
+
+    /**
+     * Sets the transform name for the step
+     * 
+     * @param {Transform} transform - the transform of the step
+     * 
+     */
+
+    set transform(transform){
+        this.transform = transform;
+    }
+
+    /**
+     * Sets the transform name for the step
+     * 
+     * @returns {Transform} transform - the transform of the step
+     * 
+     */
+
+    get transform(){
+        return this.transform;
+    }
+
 }
 
 module.exports = Step;
